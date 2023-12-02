d4bddb264d907c479ad8e8b345b90c78b489f1f3
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2023-12-02 13:48:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function replace1(text, photo) {
+    document.getElementsByClassName('hello-text')[0].innerText = text;
+    document.getElementsByClassName('photo')[0].src = photo;
+}
+
+function replace2(text, photo) {
+    document.getElementsByClassName('hello-text')[1].innerText = text;
+    document.getElementsByClassName('photo')[0].src = photo;
+}
+
+function replace3(text, photo) {
+    document.getElementsByClassName('hello-text')[2].innerText = text;
+    document.getElementsByClassName('photo')[0].src = photo;
+}
